c71f5eebdd846f33816ad72699f67c0aa7e1d1e7
Dev Ahsan Crystallite <143542001+devsahal-crystallite@users.noreply.github.com>
2024-01-25 23:59:44 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //this is used for newer version for JS, its automatically assume old code with new versions
+let accountEmail = "ahsan@abc.com"
+let age = 30
+let accountPassword = null
+
+console.log(3 + 3);
+console.log(accountEmail);
+console.log(accountPassword); 
+
+//data types
+// number => 2 to power 53
+// string = ""
+// bigint 
+// boolean = true/false
+// null is standalone type which is by default null or undefined but console and node js is consider as a object 
+// symbol = unique  => this is for uniqueness 
+
+console.log(typeof null)
---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const accoundId = "123456"
+let accountEmial = "ahsan@abc.com"
+let accountPassword = "951753"
+var accountGenerate = "123@44"
+
+//accountPassword = "00000"
+//accountGenerate = "999999"
+//accountEmial = "sahal@abc.com"
+/* 
+Var we wont use because its depreciated and the old practice. Just Use const and let 
+*/
+
+console.log(accoundId);
+
+console.table([accoundId, accountEmial, accountGenerate, accountPassword])
+
