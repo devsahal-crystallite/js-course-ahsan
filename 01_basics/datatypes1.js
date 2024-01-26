1165493d2467a6a4b6f7db892969a1dec07660c7
devsahal-crystallite <143542001+devsahal-crystallite@users.noreply.github.com>
2024-01-27 03:07:19 +0500

    learning 7

---
+++ 01_basics/datatypes1.js
@@ -0,0 +1,26 @@
+let valueInNumber = Number(32)
+let name = 'Ahsan Sahal'
+let position = "Website Developer"
+let address = 'SMCHS, karachi'
+let company = "Crytal-lite Pak Pvt Ltd"
+
+//console.log(name, position, address);
+//console.table([name, valueInNumber, position, address, company]);
+
+let gym = 1000
+
+let StringNumber = String(1000)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
+let loggin = 1
+let booleanIsLoggedIn = Boolean(loggin)
+
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+
+//data types convertions
+// humne Number datatype ko convert kia ValueInNumber k through Number me
+// StringNumber se hum convert krty hain values ko strings me
+// booleanIsLoggedIn ko use krty hain true and false convert krne k liye 
