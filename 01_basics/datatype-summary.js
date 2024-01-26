d8050e6381bcfbf554645c796678ba12b4a7af73
devsahal-crystallite <143542001+devsahal-crystallite@users.noreply.github.com>
2024-01-27 04:51:38 +0500

    datatype summary basics

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,8 @@
+// Primitive type
+/*
+7 Types of Primitive
+String, Boolean, Null, undefined, Number, BigInt, Symbol
+*/
+
+// Non Primitive Types
+// Arrays, Objects, Functions
---
+++ 01_basics/operations.js
@@ -0,0 +1,24 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+let num1 = 3
+let num2 = 5
+let num3 = num1 + num2
+let num4 = num3
+console.log(num4++);
+
+//Prefix and Post Prefix
+
+
+//Confuse comparison operators
+//where string and integars value conversions
+
+console.log(null > 2 )
+console.log(null < 2 )
+console.log(null >= 2)
+console.log(null == 2)
+
+console.log(undefined > 1)
+console.log(undefined >= 1)
+console.log(undefined == 1)
+
+//confuse operators which we avoid 
