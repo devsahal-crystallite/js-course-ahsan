6b5a1740874f56598b3b884a02820fecffd70268
devsahal-crystallite <143542001+devsahal-crystallite@users.noreply.github.com>
2024-01-27 05:54:32 +0500

    strings values

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -6,3 +6,19 @@ String, Boolean, Null, undefined, Number, BigInt, Symbol
 
 // Non Primitive Types
 // Arrays, Objects, Functions
+
+// Null return Object
+// Undefined return Undefined
+// String Return String
+// Boolean return True/false
+// Number Return Number
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// **********************************************************
+
+//Stack and Heap
+// All Primitive value stored in Stack
+// All Non-Primitive values stored in Heap
---
+++ 01_basics/string.js
@@ -0,0 +1,15 @@
+//String Values
+
+const name = "Ahsan sahal"
+const position = 'Developer'
+console.log(name)
+
+console.log(  `My Name is ${name} and i am a ${position}` )
+
+const Post = new String('Ahsansahal')
+
+console.log(Post)
+
+// new string values ko divide krdeta ha or unki length btata ha isko console kr k check ha
+
+
